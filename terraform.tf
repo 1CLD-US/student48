b9e83acd068ed12c49942e01ec4a14fad91fa9ed
Rohit Talakokkula <rtalakok@cisco.com>
2023-06-21 22:23:46 +0000

    first commit

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket         = "onecloud-kube"
+    key            = "pod48fetchckdcudufbtludibdevlidjtfkdbcbebedlcblcr
+-ec2.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
